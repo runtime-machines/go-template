77b1698807f9a962a27cf2067820da8dd6f0933b
Elia Maggioni <elia.maggioni@gmail.com>
2022-11-25 10:35:42 +0100

    fix test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func Test_greet(t *testing.T) {
-	want := "Hi!"
+	want := "Hi! :)"
 	if got := greet(); got != want {
 		t.Errorf("greet() = %v, want %v", got, want)
 	}
